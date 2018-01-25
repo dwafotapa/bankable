dbb703c4aa90d34d8fd3b07b90364c079700d7d6
dwafotapa <dominique.wafotapa@gmail.com>
2018-01-25 21:52:57 +0100

    Fix active nav link issue and republish

--- src/components/MenuWrapper/MenuWrapper.js
+++ src/components/MenuWrapper/MenuWrapper.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
-class MenuWrapper extends Component {
-  render() {
-    const { pathname } = window.location
-    return (
-      <Menu pointing secondary size="massive">
-        <Menu.Item
-          active={pathname === '/'}
-          as={Link}
-          to="/"
-        >
-          Home
-        </Menu.Item>
-        <Menu.Item
-          active={pathname.startsWith('/accounts')}
-          as={Link}
-          to="/accounts"
-        >
-          Accounts
-        </Menu.Item>
-      </Menu>
-    )
-  }
-}
+const MenuWrapper = () => (
+  <Menu pointing secondary size="massive">
+    <Menu.Item
+      as={NavLink}
+      to="/"
+      exact
+    >
+      Home
+    </Menu.Item>
+    <Menu.Item
+      as={NavLink}
+      to="/accounts"
+      exact
+    >
+      Accounts
+    </Menu.Item>
+  </Menu>
+)
 
 export default MenuWrapper
